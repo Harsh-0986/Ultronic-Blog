d765eb06bdf1b8a8682534afb862c20c3ece00f3
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-12-30 09:12:02 +0530

    Deploy to vercel

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from "next/head";
 import { PostCard, PostWidget, Categories } from "../components";
 import FeaturedPosts from "../sections/FeaturedPosts";
 import { getPosts } from "../services";
+import '../styles/globals.scss'
 
 export default function Home({ posts }) {
 	return (
 		<div className="container mx-auto px-10 mb-8">
 			<Head>
-				<title>Graph-CMS Blog</title>
+				<title>Ultronic Blog</title>
+
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<FeaturedPosts />
